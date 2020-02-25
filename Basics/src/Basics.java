197d5556cd9c342085a4203cdd8416f6a4ffd1b1
Marcoeus <macro.prois@gmail.com>
2020-02-25 11:14:02 +0100

    added principle of a Stack

--- Basics/src/Basics.java
+++ Basics/src/Basics.java
@@ -3,32 +3,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class Basics {
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		/*Read in a file*/
-		File text = new File("text.txt");		
-		Scanner scnr = new Scanner(text);
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Bryan");
+		names.push("Sue");
+		names.push("Sally");
 		
-		/*Create a list*/
-		List<String> students = new ArrayList<String>();
+		System.out.println(names.remove());
+		System.out.println(names.remove());
+		System.out.println(names.remove());
 		
-		/*Write content of file into list "students"*/
-		while(scnr.hasNextLine()) {
-			students.add(scnr.nextLine());
-		}
-		
-		/*Print the list with a for each loop*/
-		for(String name: students) 
-		{
-			System.out.printf("name: %s \n", name);
-		}	
-		
-		/*Close scanner so that it doesn't eat up memory*/
-		scnr.close();
-
 	}
 
 }
