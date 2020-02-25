71f62075455b0656496ca690cbdd5a43d86547cb
Marcoeus <macro.prois@gmail.com>
2020-02-25 13:32:52 +0100

    update contains linkedlist and stack principle

--- Basics/src/Basics.java
+++ Basics/src/Basics.java
@@ -1,9 +1,11 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 import java.io.File;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Basics {
@@ -14,11 +16,22 @@ public class Basics {
 		names.push("Bryan");
 		names.push("Sue");
 		names.push("Sally");
+		names.add(2, "Susan");
 		
-		System.out.println(names.remove());
-		System.out.println(names.remove());
-		System.out.println(names.remove());
-		
+		ListIterator<String> iter = names.listIterator();
+		/*Check if there's an element in list.
+		 * if so print actual element. This here prints the normal list 
+		 * but adds Marco before each entry. The new list is printed below. */
+		while(iter.hasNext()) {
+			iter.add("Marco");
+			System.out.println(iter.next());
+		}
+		System.out.println("---------------------");
+		/*the forEach loop cannot modify the collection. That's 
+		 * why iterators are used. This loop creates an iterator implicitly.*/
+		for(String s: names) {
+			System.out.println(s);
+		}
 	}
 
 }
